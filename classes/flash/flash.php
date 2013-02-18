d1fae806ea2f12b4827162a87ed3b3b0a9fe896f
shmel210 <dilar05@i.ua>
2013-02-18 14:23:28 +0200

    Update classes/flash/flash.php

--- classes/flash/flash.php
+++ classes/flash/flash.php
@@ -1,1 +1,23 @@
-<?php defined('SYSPATH') or die('No direct script access.');class Flash_Flash{  	protected static $instance = NULL;	public static function instance(){		if (Flash::$instance === NULL){			Flash::$instance = new Flash();		}		return Flash::$instance;	}		public function __setAttribute($name,$value) {		$data = array();		$data[$name] = $value;		Session::instance()->set('flash_message',$data);	}		public function __get($name) {		$flash = Session::instance()->get('flash_message');						if(!empty($flash[$name])) {			Session::instance()->delete('flash_message');			return $flash[$name];					}		return null;	}}
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Flash_Flash{  
+	protected static $instance = NULL;
+
+	public static function instance(){
+		if (Flash::$instance === NULL){
+			Flash::$instance = new Flash();
+		}
+		return Flash::$instance;
+	}
+	
+	public function __setAttribute($name,$value) {
+		$data = array();
+		$data[$name] = $value;
+		Session::instance()->set('flash_message',$data);
+	}
+	
+	public function __get($name) {
+		$flash = Session::instance()->get_once('flash_message');				
+		return (isset($flash[$name]))?$flash[$name]:null;
+	}
+}
